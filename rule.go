605d65d299fe57d1af7c30c4909e6645d3d3f0f4
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-07-07 16:19:12 +0800

    fix: from.id reflect

--- rule.go
+++ rule.go
@@ -24,7 +24,7 @@ func newctrl(service string, o *ctrl.Options[*Ctx]) Rule {
 		if !ctx.Message.Chat.IsPrivate() {
 			gid = ctx.Message.Chat.ID
 		}
-		return c.Handler(uintptr(unsafe.Pointer(ctx)), gid, reflect.ValueOf(ctx.Value).FieldByName("From").FieldByName("ID").Int())
+		return c.Handler(uintptr(unsafe.Pointer(ctx)), gid, reflect.ValueOf(ctx.Value).Elem().FieldByName("From").FieldByName("ID").Int())
 	}
 }
 
